efe4f794e2bcb8eeca509190c9b1d976c0c8dc7f
James Carroll <jamcar23@gmail.com>
2020-01-30 19:39:57 -0600

    feat(extensions): add initial extension to MvcOptions to add both formatters

---
+++ src/AspCoreProtobufFormatters/Extensions/StartupExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCoreProtobufFormatters.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddProtobufFormatters(this MvcOptions mvc)
+        {
+            mvc.InputFormatters.Insert(0, new ProtobufInputFormatter());
+            mvc.OutputFormatters.Insert(0, new ProtobufOutputFormatter());
+        }
+    }
+}
